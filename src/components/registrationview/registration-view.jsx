2432d1fd99da873adfab018fdb8ba909bf215e66
Justin Goats <justingoats@yahoo.com>
2021-06-01 21:06:52 -0700

    created registration.jsx component

---
+++ src/components/registrationview/registration-view.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class RegistrationView extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
