bd1a07e008b5bc592cab8c2375cb3e93ee0a2c76
Justin Goats <justingoats@yahoo.com>
2021-06-09 17:47:27 -0700

    added responsiveness to movieview, mainview, and movie-card components

--- src/components/moviecard/movie-card.js
+++ src/components/moviecard/movie-card.js
@@ -8,11 +8,18 @@ export default class MovieCard extends React.Component {
     render() {
         const { movie, onBackClick } = this.props;
         return (
-            <Card>
+            <Card className="card">
                 <Card.Img className="movie-img" variant="top" src={"/src/images/" + movie.imageURL} />
                 <Card.Body>
                     <Card.Title>{movie.title}</Card.Title>
                     <Card.Text>{movie.description}</Card.Text>
+                    <Card.Title>Genre</Card.Title>
+                    <Card.Text>{movie.genre.name}</Card.Text>
+                    <Card.Text>{movie.genre.description}</Card.Text>
+                    <Card.Title>Director</Card.Title>
+                    <Card.Text>{movie.director.name}</Card.Text>
+                    <Card.Text>{movie.director.bio}</Card.Text>
+                    <Card.Text>{movie.director.born}</Card.Text>
                     <Button onClick={() => onBackClick(movie)} variant="link">Close</Button>
                 </Card.Body>
             </Card>
