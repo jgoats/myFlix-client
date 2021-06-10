52d8131c6cd0d82bfa32b109dcef11917df3c74e
Justin Goats <justingoats@yahoo.com>
2021-06-10 07:42:03 -0700

    uncommended out line of code preventing axios from handling get request

--- src/components/loginview/login-view.js
+++ src/components/loginview/login-view.js
@@ -4,6 +4,7 @@ import Form from 'react-bootstrap/Form';
 import Button from "react-bootstrap/Button";
 import "./login-view.scss";
 import MovieTicket from "/src/images/movie-ticket.svg";
+import axios from 'axios';
 
 
 export default class LoginView extends React.Component {
